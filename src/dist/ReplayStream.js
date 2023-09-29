42f47e57c6a13c447b2ef42ceafacc58b9510620
ownxcmd <66704279+ownxcmd@users.noreply.github.com>
2023-09-29 09:19:45 -0400

    Wait before starting display

--- src/dist/ReplayStream.js
+++ src/dist/ReplayStream.js
@@ -1,16 +1,18 @@
 import { Replay } from './Replay.js'
 
 class ReplayStream extends Replay {
-    constructor(renderer, tickRate) {
-        super(renderer, tickRate);
+    constructor(renderer) {
+        super(renderer);
         this.socket = new WebSocket('ws://localhost:8080');
 
         this.socket.addEventListener('message', (event) => {
             const data = JSON.parse(event.data);
             //console.log(data);
             this.queue.push(...data);
-            super.displayLoop();
-        })
+            if (this.queue.length >= (1/this.tickDelay)*10) {
+                super.displayLoop();
+            }
+        });
     }
 
     destroy() {
