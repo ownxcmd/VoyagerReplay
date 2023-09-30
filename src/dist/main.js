e6e274280264244cd949192823cdf6b51e64f3a3
ownxcmd <66704279+ownxcmd@users.noreply.github.com>
2023-09-29 23:54:23 -0400

    Move select replay code to main

--- src/dist/main.js
+++ src/dist/main.js
@@ -1,30 +1,79 @@
 import * as THREE from 'three';
 import { ReplayStream } from './ReplayStream.js';
-
-const TICKS_PER_SECOND = 30;
+import { ReplayFile } from './ReplayFile.js';
 
 const renderer = new THREE.WebGLRenderer();
 renderer.shadowMap.enabled = true;
 renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 document.body.appendChild( renderer.domElement );
 
-let ActiveReplay = new ReplayStream(renderer, 1/TICKS_PER_SECOND);
+const streamSocket = new WebSocket('ws://localhost:8080');
+
+let ActiveReplay = new ReplayStream(renderer);
 
 function SwitchReplay(Replay) {
     if (ActiveReplay) {
+        streamSocket.removeEventListener('message', ActiveReplay);
         ActiveReplay.destroy();
     }
+
+    streamSocket.addEventListener('message', Replay);
     ActiveReplay = Replay;
 }
 
 function animate() {
-    const display = ActiveReplay.display;
+    if (ActiveReplay) {
+        const display = ActiveReplay.display;
+        display.controls.update();
+        display.renderer.setSize( window.innerWidth, window.innerHeight );
+        display.renderer.render( display.scene, display.camera );
+    }
 
     requestAnimationFrame( animate );
-    display.controls.update();
-    display.renderer.setSize( window.innerWidth, window.innerHeight );
-    display.renderer.render( display.scene, display.camera );
 }
+
+// replay selection
+function selectReplay (){
+    return new Promise(resolve => {
+        const input = document.createElement('input');
+        input.type = 'file';
+        input.multiple = false;
+        input.accept = 'application/json';
+
+        input.onchange = _ => {
+            const file = input.files[0];
+            const reader = new FileReader();
+
+            reader.readAsText(file,'UTF-8');
+            reader.onload = readerEvent => {
+                const content = readerEvent.target.result;
+                resolve(content);
+            }
+        };
+
+        input.click();
+    });
+}
+
+// Replay select callback
+async function onButtonClicked(){
+    const selectedReplay = await selectReplay();
+    if (!selectedReplay) {
+        return;
+    }
+
+    let replayData;
+    try {
+        replayData = JSON.parse(selectedReplay);
+    } catch (e) {
+        alert('Invalid JSON replay file provided');
+    }
+
+    const NewReplay = new ReplayFile(renderer, replayData);
+    SwitchReplay(NewReplay);
+}
+
+
 
 console.log(window.location.href.split('/').slice(-1));
 animate();
